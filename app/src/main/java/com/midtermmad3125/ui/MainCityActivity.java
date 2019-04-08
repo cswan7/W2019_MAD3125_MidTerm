08443698302ed4946255f4e0f7c303ef77a022e1
macstudent <chandanswan@gmail.com>
2019-04-08 19:15:23 -0400

    working with JSON

--- app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
@@ -1,24 +1,49 @@
 package com.midtermmad3125.ui;
 
+import android.app.LauncherActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.midtermmad3125.R;
 import com.midtermmad3125.utils.ReadJSONUtils;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class MainCityActivity extends AppCompatActivity
 {
-//hello
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
+    private List<ListItem> listItems;
+    //hello
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void processJSON()
+    {
+        String jsonString = loadJSONFromAsset();
+        if(jsonString!=null){
+            try{
+                JSONObject mJSONObject = new JSONObject();
+                JSONArray mJSONArray = mJSONObject.getJSONArray("list");
+                for (int i=0;i<mJSONArray.length();i++)
+                    
+
+
+            }
+        }
     }
 
     private String loadJSONFromAsset() {
