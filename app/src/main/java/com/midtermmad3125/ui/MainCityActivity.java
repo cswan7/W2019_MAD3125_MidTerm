3135ea310a14a3b7c7c1839141785194be0b38e1
macstudent <chandanswan@gmail.com>
2019-04-08 18:41:19 -0400

    some file added

--- app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
@@ -7,6 +7,10 @@ import android.util.Log;
 import com.midtermmad3125.R;
 import com.midtermmad3125.utils.ReadJSONUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 public class MainCityActivity extends AppCompatActivity
 {
 //hello
@@ -15,5 +19,21 @@ public class MainCityActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private String loadJSONFromAsset() {
+        String json;
+        try {
+            InputStream is = getAssets().open("moscow_weather.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
